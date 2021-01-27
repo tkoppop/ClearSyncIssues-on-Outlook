7e6975c8e81c7be52a8062316ba77489d107c755
Lucas Wung <59458611+tkoppop@users.noreply.github.com>
2021-01-27 15:48:25 -0800

    Add files via upload
    First try using the remove command and a for loop. Cycles through the ComObject $ol. 
    (sometimes throws an exception)

---
+++ ClearSyncIssuesV0.ps1
@@ -0,0 +1,34 @@
+﻿$o = New-Object -comobject outlook.application
+$n = $o.GetNamespace(“MAPI”)
+$Account = $n.Folders | ? { $_.Name -eq "$env:UserName" + "@glotmansimpson.com" };
+
+$Folder = $Account.Folders | ? {$_.Name -match 'Test'};
+
+$SyncIssues = $Account.Folders | ? { $_.Name -match 'Sync Issues' };
+$LocalFailures = $SyncIssues.Folders | ? { $_.Name -match 'Local Failures' };
+$Conflicts = $SyncIssues.Folders | ? { $_.Name -match 'Conflicts' };
+
+$Count1 = $SyncIssues.Items().Count
+$Count1
+$SyncIssues = $SyncIssues.Items()
+
+For ($i = ($Count1);$i -ge 1;$i--) {
+    $SyncIssues.Remove($i)
+}
+
+$Count2 = $Conflicts.Items().Count
+$Count2
+$Conflicts = $Conflicts.Items()
+
+For ($i = ($Count2);$i -ge 1;$i--) {
+    $Conflicts.Remove($i)
+}
+
+$Count3 = $LocalFailures.Items().Count
+$Count3 
+$LocalFailures = $LocalFailures.Items()
+
+For ($i = ($Count3);$i -ge 1;$i--) {
+    $LocalFailures.Remove($i)
+}
+
