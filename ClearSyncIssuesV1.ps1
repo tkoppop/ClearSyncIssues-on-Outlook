5e33a7cece95db2e1cddcbdf9f54da91f2e128c8
Lucas Wung <59458611+tkoppop@users.noreply.github.com>
2021-01-28 14:33:50 -0800

    Add files via upload
    Updated version using the delete function. However it is still a hassle to clear the items in the trashbin.

---
+++ ClearSyncIssuesV1.ps1
@@ -0,0 +1,20 @@
+﻿$o = New-Object -comobject outlook.application
+$n = $o.GetNamespace(“MAPI”)
+$Account = $n.Folders | ? { $_.Name -eq "$env:UserName" + "@glotmansimpson.com" };
+
+$TestFolder = $Account.Folders | ? {$_.Name -match 'Test'};
+
+$SyncIssues = $Account.Folders | ? { $_.Name -match 'Sync Issues' };
+$LocalFailures = $SyncIssues.Folders | ? { $_.Name -match 'Local Failures' };
+$Conflicts = $SyncIssues.Folders | ? { $_.Name -match 'Conflicts' };
+while ($SyncIssues.Items.Count -ne 0){
+$SyncIssues.Items | % {$_.delete()}
+}
+
+while ($LocalFailures.Items.Count -ne 0){
+$LocalFailures.Items | % {$_.delete()}
+}
+
+while ($Conflicts.Items.Count -ne 0){
+$Conflicts.Items | % {$_.delete()}
+}
