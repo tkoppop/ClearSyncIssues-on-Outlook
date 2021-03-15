cfa904ecf5124ff98a80b1be3ff536438ee6fedd
Lucas Wung <59458611+tkoppop@users.noreply.github.com>
2021-03-15 12:29:37 -0700

    Update ClearSyncIssuesV3.ps1

--- ClearSyncIssuesV3.ps1
+++ ClearSyncIssuesV3.ps1
@@ -11,54 +11,47 @@ $Conflicts = $SyncIssues.Folders | ? { $_.Name -match 'Conflicts' };
 $Count1 = $SyncIssues.Items().Count
 $Count1
 $SyncIssues = $SyncIssues.Items()
-
 while ($SyncIssues.Items.Count -ge 0){
-
-try{
-	For ($i = ($Count1);$i -ge 1;$i--) {
-   		$SyncIssues.Remove($i)
+	try{
+		For ($i = ($Count1);$i -ge 1;$i--) {
+   			$SyncIssues.Remove($i)
+		}
+	} catch {
+		break
 	}
-} catch {
-break
-}
-if($Count1 -eq 0){
-
-break
-}
+	if($Count1 -eq 0){
+		break
+	}
 }
 
 $Count2 = $Conflicts.Items().Count
 $Count2
 $Conflicts = $Conflicts.Items()
-
 while ($Conflicts.Items.Count -ge 0){
-try{
-	For ($i = ($Count2);$i -ge 1;$i--) {
-    		$Conflicts.Remove($i)
+	try{
+		For ($i = ($Count2);$i -ge 1;$i--) {
+    			$Conflicts.Remove($i)
+		}
+	} catch {
+		break
 	}
-} catch {
-break
-}
-if($Count2 -eq 0){
-
-break
-}
+	if($Count2 -eq 0){
+		break
+	}
 }
 
 $Count3 = $LocalFailures.Items().Count
 $Count3 
 $LocalFailures = $LocalFailures.Items()
-
 while ($LocalFailures.Items.Count -ge 0){
-try{
-	For ($i = ($Count3);$i -ge 1;$i--) {
-    		$LocalFailures.Remove($i)
+	try{
+		For ($i = ($Count3);$i -ge 1;$i--) {
+    			$LocalFailures.Remove($i)
+		}
+	} catch {
+		break
 	}
-} catch {
-break
-}
-if($Count3 -eq 0){
-
-break
-}
+	if($Count3 -eq 0){
+		break
+	}
 }
